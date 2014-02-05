f682523ced276b3648d39a300070e2b79bf38805
yodadog <jhardin@cs.csubak.edu>
2014-02-05 13:35:46 -0800

    Print out single digit matrix

--- Digitizer.cpp
+++ Digitizer.cpp
@@ -12,15 +12,19 @@ using namespace std;
 using namespace cv;
 
 int main() {
-	CvMLData data; //csv data object
+	CvMLData data; 					//csv data object
 	data.read_csv("./training_data/train.csv");
 	data.set_response_idx(0);
-	Mat x; //Matrix to hold all the digit data
+	
+	//Matrix to hold all the digit data
+	Mat x; 						
 	x=data.get_values();
 	cout << "Rows: " << x.rows << " Cols: " << x.cols << endl;
 
 	IplImage * img = cvCreateImage(Size(28,28),IPL_DEPTH_32F, 1);
 
+	for (int i=0; i< 784; i+=28)
+		cout << x(Range(4,5),Range(i,i+28)) << endl;//.colRange(i, i*28) << endl; 
 	/* //show csv data
 	cout << x;
 	cout << endl;
